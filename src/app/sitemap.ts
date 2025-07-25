1b5ddfadb8e178d86e2de924b2f2c60c11e5ee4a
JagdishSuthar1 <jagdishsuthar4581@gmail.com>
2025-07-25 22:45:20 +0530

    Update env variables

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,10 @@ import { MetadataRoute } from "next";
 export default async function sitemap() : Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url : `${process.env.NEXT_PUBLIC_URL}/about`
+            url : `${process.env.NEXT_PUBLIC_BASE_URL}/about`
         },
         {
-            url : `${process.env.NEXT_PUBLIC_URL}/dashboard`
+            url : `${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`
         }
     ]
 }
