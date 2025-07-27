0063e01bd36c5739d886f56ac0e44e6de2852969
Jagdish <158441753+JagdishSuthar1@users.noreply.github.com>
2025-07-27 22:50:29 +0530

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,47 @@
 import { prisma } from "@/lib/prisma";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 const handler = NextAuth({
-    providers : [
-        GoogleProvider(
-            {
-                clientId : process.env.GOOGLE_CLIENT_ID as string,
-                clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
-            }
-        )
-    ]
-    ,
-    secret : process.env.NEXTAUTH_SECRET,
-    session: {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
     strategy: "database",
   },
-  cookies: {
-    sessionToken: {
-      name: "__Secure-next-auth.session-token",
-      options: {
-        httpOnly: true,
-        sameSite: "lax", 
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-      },
+  useSecureCookies: process.env.NODE_ENV === "production",
+  trustHost: true,
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { email: user.email! },
+        });
+
+        if (!existingUser) {
+          await prisma.user.create({
+            data: {
+              email: user.email!,
+              provider: "Google",
+              role: "EndUser",
+            },
+          });
+        }
+      } catch (err) {
+        console.error("SignIn Error:", err);
+      }
+      return true;
     },
   },
-  useSecureCookies: process.env.NODE_ENV === "production",
-    trueHost : true,
-    callbacks : {
-        async signIn(params) {
-            try {
-                await prisma.user.create({
-                    data : {
-                        email : params.user.email!,
-                        provider : "Google",
-                        role : "EndUser"
-                    }
-                })
-            }
-            catch(err) {
-                console.log(err)
-            }
-            return true
-        }
-    }
-    ,
-    pages : {
-        signOut :"/",
-        // signIn : 
-    }
+  pages: {
+    signOut: "/",
+  },
+});
 
-})
-
-
-export {handler as GET , handler as POST}
+export { handler as GET, handler as POST };
