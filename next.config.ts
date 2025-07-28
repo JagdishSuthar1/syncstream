eb496dc708bd4e7d0d6c08a89932c7bc41ff0a1a
JagdishSuthar1 <jagdishsuthar4581@gmail.com>
2025-07-28 23:46:19 +0530

    Update next config and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
-  transpilePackages: ['@prisma/client'],
+
 };
 
 export default nextConfig;
